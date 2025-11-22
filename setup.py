e5ac33d6f0d4bd1564292fc31c3eab3489b302fa
Rishabh Raj <nyxveil78@gmail.com>
2025-11-22 18:26:36 +0530

    Add setup.py for mastermath package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='mastermath',
+    version='1.0.0',
+    py_modules=['mastermath'],
+    description='A math helper chatbot for students of class 8',
+    author='Rishabh Raj',
+)
